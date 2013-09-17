321d9d417eab9c853f38031a7af11be276d95948
Anthony Cole <anthony.cole0001@gmail.com>
2013-09-17 15:56:57 -0400

    added main index page with styles and picture

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+	<meta content="CSCI-E15 Project 1 Homework" name="description" />
+	<title>CSCI-E15 Project 1 Homework - Anthony Cole</title>
+	<meta content="Setting up first PHP website" name="keywords" />
+	<link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+	<body>
+		<!-- website authors name -->
+		<br />
+		<br />
+		<br />
+		<div id= "main">
+			<h1 align="center"><strong>Anthony Cole</strong></h1>
+			<div id="mainImage">
+			<img id="Image1" src="anthony.png" style="border-width:0px;height:150px;width:150px;" /></a>
+			</div>
+		<br />
+		<br />
+		<br />
+			<h1><strong>About me:</strong></h1>
+			<p>I grew up in the Maryland suburbs just outside of Washington DC.  My professional career spans sales, business development, and consulting within 3 startups (one of which I co-founded) and 3 tours of duty in large enterprise software companies (Oracle, IBM and SAP).  I love technology and it's ability to improve our lives and the experiences we share with the people we care about.</p>
+			<br />
+			<h1><strong>Education:</strong></h1>
+			<p>I graduated with a Bachelors of Arts Degree in Behavioral and Social Sciences from the University of Maryland College Park.   I'm three courses into the Harvard's Extension School, where I am seeking a Masters of Liberal Arts in Software Engineering.   My goal is to leverage the masters degree to further my career in product management.</p><br />
+			<h1><strong>Personal Life:</strong></h1>
+			<p>I live in Cambridge, MA and I am married to Jaclyn Grimshaw, who works in management consulting.   We have a beautiful three year old son name Bennett.   He is a smart, creative and rambunctious bundle of energy as well as a source of great joy in our lives.</p>
+			<br />
+			<h1><strong>Interests:</strong></h1>
+			<p>I'm an avid follower of all things related to entrepreneurship.   I previously cofounded an enterprise startup called Cogent Mobile and I'm currently working on a new project on the consumer big data analytics.   I have a broad interest in user experience design of various technology sectors spanning Quantified self, Big Data, SaaS, consumer analytics.   I love working-out as often as I can make it to the gym, and specifically enjoy running and cycling.  
+			</p>
+			<br />
+			<br />
+			<br />
+
+
+
+		<footer>
+    		<p>Web page authored by Anthony Cole using Sublime Text2 for editing and git for version control.</p>
+    		<p><a href="http://validator.w3.org/check/referer">Validate Markup</a>
+            &nbsp;|&nbsp;<a href="http://jigsaw.w3.org/css-validator/check/referer">Validate CSS</a>
+    		</p>
+		</footer>
+		</div>
+	</body>
+</html>
